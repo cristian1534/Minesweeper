a7851bac8201c35dab5635104df78e330f538453
Cristian Machuca <cmachuca32@gmail.com>
2021-04-19 05:02:38 +0300

    Test...

--- Minesweeper/minesweeper/src/pages/Admin.js
+++ Minesweeper/minesweeper/src/pages/Admin.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useMemo, memo } from 'react';
 import { IoTrashBinSharp } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
