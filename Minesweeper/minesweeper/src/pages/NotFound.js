2cf89e7779ee4a21635692e7d121709f9f230a9e
Cristian Machuca <cmachuca32@gmail.com>
2021-04-22 19:33:08 +0300

    Error 404 updated.

--- Minesweeper/minesweeper/src/pages/NotFound.js
+++ Minesweeper/minesweeper/src/pages/NotFound.js
@@ -6,7 +6,6 @@ const NotFound = () => {
         <div>
              <h3>Error 404</h3>
             <h1>NOT FOUND</h1>
-            <img src="https://android4us.ru/wp-content/uploads/logo-minesweeperclassicsaper-512x400.png" alt="not-found"/>         
         </div>
     );
 }
